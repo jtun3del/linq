60bf56f99de03dbf8e88a47ce1b6148d4c57686f
Jacob Tunnicliff <jtunnicliff@my.wctc.edu>
2025-10-21 13:58:28 -0500

    1.25b

--- Program.cs
+++ Program.cs
@@ -187,8 +187,8 @@ Console.Clear();
 //   Console.WriteLine($"{obj.Name}, alias(s):\n\t{String.Join(", ", obj.Alias)} ");
 // }
 
- Console.WriteLine($"it is {marios.Count(c => c.Species.Contains("Human"))}");
+//  Console.WriteLine($"it is {marios.Count(c => c.Species.Contains("Human"))}");
 
-// foreach(var obj in dks.Where(c => c.Species.Contains("Kremling"))){
-//   Console.WriteLine($"{obj.Name}");
-// }
+foreach(var obj in marios.Where(c => c.Species.Contains("human"))){
+  Console.WriteLine($"{obj.Name}");
+}
